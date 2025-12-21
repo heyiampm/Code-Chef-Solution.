1c82bb7fa1f9e13bf5dd3324431a5f2b10ca3a73
Pm Pranta <heyiampm@gmail.com>
2025-12-21 22:03:22 +0600

    Code Solution

--- Solution/Day4.cpp
+++ Solution/Day4.cpp
@@ -11,7 +11,7 @@ int main() {
           while (n > 0) {
                     int lastDigit = n % 10;     // Get the last digit
                     sum = sum + lastDigit;                  // Add digit to sum
-
+                    
                     product = product * lastDigit; // Multiply digit to product
 
                     n = n / 10;               // Remove the last digit
