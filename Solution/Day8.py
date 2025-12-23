105473ff8b9ae2dc5337bc96db5ea1f5b8b16617
Pm Pranta <heyiampm@gmail.com>
2025-12-23 19:21:38 +0600

    Code Solution

---
+++ Solution/Day8.py
@@ -0,0 +1,10 @@
+# code chef problem 
+
+for i in range(1,6):
+          print(i,'-',i*i)
+
+num = 2
+for i in range(num):
+          for j in range(num):
+                    print("****")
+                    print()
