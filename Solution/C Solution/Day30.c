17c326bc3b78075a7b5d81c7824556c03eb8e5cc
Pm Pranta <heyiampm@gmail.com>
2025-12-26 11:45:59 +0600

    Solution C

---
+++ Solution/C Solution/Day30.c
@@ -0,0 +1,42 @@
+
+
+// //Pass-Fail Checker
+
+// #include <stdio.h>
+
+// int main() {
+//     int marks;
+//     printf("Enter your marks: ");
+//     scanf("%d", &marks);
+
+//     if (marks >= 50) {
+//         printf("You passed!\n");
+//     } else {
+//         printf("You failed.\n");
+//     }
+
+//     return 0;
+// }
+// ////////Largest of Three
+// #include <stdio.h>
+
+// int main() {
+//     int num1, num2, num3;
+//     scanf("%d %d %d", &num1, &num2, &num3);
+
+//     // Check and print the largest number
+//     if (num1 >= num2 && num1 >= num3) {
+//         printf("Largest: %d\n", num1);
+//     } else if (num2 >= num1 && num2 >= num3) {
+//         printf("Largest: %d\n", num2);
+//     } else {
+//         printf("Largest: %d\n", num3);
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
